0930da3b8b81b546426f5aee9a18c5900a1c4a31
pedro.lopes <pedro.lopes@mv.com.br>
2019-07-10 09:17:09 -0300

    chore: Configuraçao do processo de build

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,21 @@
 import pkg from './package.json';
 import { terser } from "rollup-plugin-terser";
+import babel from 'rollup-plugin-babel';
 export default {
   input: './index.js', // our source file
   output: [
     {
       file: 'dist/bundle.js',
-      format: 'esm'
+      format: 'umd'
     },
   ],
   external: [
     ...Object.keys(pkg.dependencies || {})
   ],
   plugins: [
-    terser()
+    terser(),
+    babel({
+      exclude: 'node_modules/**'
+    })
   ]
 };
