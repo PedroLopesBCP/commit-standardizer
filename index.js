f63af64b3e8636ce9bce1ab88bb54d5b94c3cc9c
pedro.lopes <pedro.lopes@mv.com.br>
2019-07-10 13:40:10 -0300

    feat: Adiçao de novo prompt

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const childProcessExec = require('child_process').exec;
 const util = require('util');
 const exec = util.promisify(childProcessExec);
 
-/**
- * Função responsável por montar o objeto com os atributos do commit
- */
+let commit = { commitType: '', commitMessage: '', commitReferences: '', pushToOrigin: true };
+
+// Private methods ----------------------------------------------------
 const mountCommit = async () => {
   const commitType = await prompts({
     type: 'select',
@@ -36,7 +36,13 @@ const mountCommit = async () => {
     message: 'Insira os tickets referenciados separados por vírgula'
   });
 
-  let mountedCommit = {};
+  const pushToOrigin = await prompts({
+    type: 'toggle',
+    name: 'value',
+    message: 'Enviar as alterações para o repositório remoto?'
+  })
+
+  let mountedCommit = { commitType: '', commitMessage: '', commitReferences: '', pushToOrigin: true };
 
   if (commitType.value) {
     mountedCommit.commitType = commitType.value;
@@ -47,45 +53,63 @@ const mountCommit = async () => {
   if (commitReferences.value) {
     mountedCommit.commitReferences = commitReferences.value
   }
+  mountedCommit.pushToOrigin = pushToOrigin.value;
 
   return mountedCommit;
-
 }
 
-let commit = {};
+async function _stageAllChanges() {
+  return await exec('git add .');
+}
 
-/**
- * Função responsável por enviar as alterações realizadas
- */
-const stageAllChanges = async () => {
-  await exec('git add .');
+async function _commitHandler(commitObject) {
+  if (commitObject.commitReferences) {
+    return commitChangesWithReferences(commitObject);
+  }
+  commitChanges(commitObject);
 }
-/**
- * Função responsável por retornar o objeto do commit
- */
-const getCommitMessage = async () => {
-  commit = await mountCommit();
-  return commit;
-}
+
 /**
  * Função responsável por realizar o commit das alterações
+ * @param commitObject
  */
-const commitChanges = async (commitObject) => {
+const commitChanges = async (commitObject = { commitType: '', commitMessage: '', commitReferences: '', pushToOrigin: true }) => {
   let message = '';
   if (commitObject) {
     message = `${commitObject.commitType} ${commitObject.commitMessage}`
   } else {
     message = `${commit.commitType} ${commit.commitMessage}`
   }
-  await exec(`git commit -m "${message}"`);
+  await _stageAllChanges();
+  return await exec(`git commit -m "${message}"`);
 }
 /**
  * Função por realizar o commit referenciando alguns tickets do JIRA
+ * @param commitObject 
  */
-const commitChangesWithReferences = async () => {
-  const message = `${commit.commitType} ${commit.commitMessage}`
-  await exec(`git commit -m "${message}" -m "${commit.commitReferences}"`);
+const commitChangesWithReferences = async (commitObject = { commitType: '', commitMessage: '', commitReferences: '', pushToOrigin: true }) => {
+  let message = '';
+  if (commitObject) {
+    message = `${commitObject.commitType} ${commitObject.commitMessage}`
+  } else {
+    message = `${commit.commitType} ${commit.commitMessage}`
+  }
+  await _stageAllChanges();
+  return await exec(`git commit -m "${message}" -m "${commit.commitReferences}"`);
 }
+// --------------------------------------------------------------------
+
+/**
+ * Função responsável por retornar o objeto do commit
+ */
+const getCommitMessage = async () => {
+  commit = await mountCommit();
+  await _commitHandler(commit);
+  if (commit.pushToOrigin) {
+    pushChanges();
+  }
+}
+
 /**
  * Função responsável por enviar as mudanças para o repostitório
  */
@@ -94,9 +118,10 @@ const pushChanges = async () => {
   await exec(`git push origin ${branch.stdout}`);
 }
 
-exports.stageAllChanges = stageAllChanges;
-exports.mountCommit = mountCommit;
 exports.getCommitMessage = getCommitMessage;
-exports.commitChangesWithReferences = commitChangesWithReferences;
-exports.commitChanges = commitChanges;
-exports.pushChanges = pushChanges;
+
+/* TODO: Analisar necessidade dentro do fluxo atual
+* exports.commitChangesWithReferences = commitChangesWithReferences;
+* exports.commitChanges = commitChanges;
+* exports.pushChanges = pushChanges;
+*/
--- scripts/commit.js
+++ scripts/commit.js
@@ -1,11 +1,5 @@
-const sendToStaging = require('../index').stageAllChanges;
 const getCommit = require('../index').getCommitMessage;
-const commit = require('../index').commitChanges;
-const push = require('../index').pushChanges;
 
 (async () => {
-  await sendToStaging();
-  const commitObject = await getCommit();
-  await commit(commitObject);
-  await push();
+  await getCommit();
 })();
