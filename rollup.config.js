35aa0cf78e8a82d4b4c2ecbe4fbb628e5d68cfbc
pedro.lopes <pedro.lopes@mv.com.br>
2019-07-10 08:58:45 -0300

    chore: Testes no processo de importaçao

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,17 @@
 import pkg from './package.json';
+import { terser } from "rollup-plugin-terser";
 export default {
   input: './index.js', // our source file
   output: [
     {
       file: 'dist/bundle.js',
-      format: 'cjs'
+      format: 'esm'
     },
   ],
   external: [
     ...Object.keys(pkg.dependencies || {})
   ],
+  plugins: [
+    terser()
+  ]
 };
